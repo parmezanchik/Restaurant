5ce469a6fa98d9dd5a716205b2ae3b81eda1bcd5
vlad <vlad.g.ua04@gmail.com>
2023-10-11 18:32:35 +0300

    explain commit

--- src/main/kotlin/MenuItem.kt
+++ src/main/kotlin/MenuItem.kt
@@ -1,3 +1,3 @@
-class MenuItem(val name: String, val description:String, val price: Float) {
+class MenuItem(val name: String, val description:String, val price: Float) {  //приклад класу для страв
 
 }
--- src/main/kotlin/Order.kt
+++ src/main/kotlin/Order.kt
@@ -1,11 +1,12 @@
 import kotlin.random.Random
 class Order(private val orderId: Int, private val tableNum: Int) {
-    private val items = mutableListOf<MenuItem>()
+    private val items = mutableListOf<MenuItem>()  //робимо змінювальний список який буде приймати страви
     private val waiters = listOf("Vlad", "Ilya", "Maks")
-    private val statuses = listOf("Cooking", "Accepted", "Ready to go")
+    private val statuses = listOf("Cooking", "Accepted", "Ready to go")  //списки для покращення роботи програми
     private val randomWaiter = waiters[Random.nextInt(waiters.size)]
-    private val randomStatus = statuses[Random.nextInt(statuses.size)]
+    private val randomStatus = statuses[Random.nextInt(statuses.size)]   //рандом офіціантів та часу готовки
     override fun toString(): String {
+        //повертаємо на екран користувача інформацію про стави
         return "Order ID: $orderId, Table: $tableNum, Waiter: $randomWaiter, Status: $randomStatus, Items: $items"
     }
 }
