30279a07e005f9792452fee3e3092a29a68c0799
Cilb <maximvitokhin@gmail.com>
2023-10-15 20:13:41 +0300

    Clients class

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,13 @@
 fun main() {
-    val order1 = Order(1, 2)
-    val order2 = Order(2, 3)
-
-    println(order1)  // Вывод информации о заказе 1
-    println(order2)  // Вывод информации о заказе 2
+    val client = Clients("Ivan",1)
+    client.add_price(2.toFloat())
+    client.add_price(3.toFloat())
+    client.add_price(2.4.toFloat())
+    client.add_list_dishes("Makarony")
+    client.add_list_dishes("Makarony")
+    client.add_list_dishes("salat")
+    println("Інформація о клієнті ${client.name} " +
+            "\nСтолик -> №${client.table_num} " +
+            "\nСума заказу -> ${client.total_price} " +
+            "\nСписок замовлених страв -> ${client.list_of_dishes}.")
 }
