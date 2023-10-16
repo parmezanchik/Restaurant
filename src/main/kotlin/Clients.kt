ea0d430c8b706eaa4c4709866ecd5fb21b3983c3
Cilb <maximvitokhin@gmail.com>
2023-10-16 22:36:45 +0300

    Fix class Clients

--- src/main/kotlin/Clients.kt
+++ src/main/kotlin/Clients.kt
@@ -1,6 +1,6 @@
 //Клас клієнтів, вхідні дані: Ім'я, тип String; Номер столика, цілочисельний тип; Остаточна ціна, тип Float; Список страв, тип String
 //У класі є методи для розрахунку ціни та списку страв.
-class Clients(var name : String,var table_num : Int,var total_price: Float = 0.toFloat(),var list_of_dishes : String = "") {
+class Clients(var name : String,var table_num : Int,var total_price: Float = 0.toFloat(),var list_of_dishes : List<String> = emptyList()) {
 
     //Функція розрахунку ціни, приймає значення ціни і сумує до основної змінної
     fun add_price(price : Float) {
@@ -12,7 +12,7 @@ class Clients(var name : String,var table_num : Int,var total_price: Float = 0.t
 
     //Функція створення листу з назв страв, приймає значення страви і додає до строки
     fun add_list_dishes(dish : String) {
-        list_of_dishes += "$dish "
+        list_of_dishes += dish
     }
 
 }
